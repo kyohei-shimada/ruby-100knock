376f082cf3648925f0f834f6832b8a55313ad7b6
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-03-06 13:53:40 +0900

    非単語形成文字でマッチするようにした

--- questions/q04.rb
+++ questions/q04.rb
@@ -1,5 +1,5 @@
 sentence = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
-words = sentence.delete(".,").split
+words = sentence.gsub(/\W+/, ',').split
 
 words.map.with_index(1) {|word, index|
   [1,5,6,7,8,9,15,16,19].include?(index) ? [word[0], word] : [word[0..1], word]
