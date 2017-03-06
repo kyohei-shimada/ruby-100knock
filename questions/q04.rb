1cedc269c7f8b0a4fe469a355c5d4a63691815d1
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-03-06 17:45:03 +0900

    oops

--- questions/q04.rb
+++ questions/q04.rb
@@ -2,5 +2,5 @@ sentence = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Mig
 words = sentence.gsub(/\W+/, ' ').split
 
 words.map.with_index(1) {|word, index|
-  [1,5,6,7,8,9,15,16,19].include?(index) ? [word[0], word] : [word[0..1], word]
+  [1,5,6,7,8,9,15,16,19].include?(index) ? [word[0], index] : [word[0..1], index]
 }.to_h
