c69a7de77cbbb7430ed5acd08d66e65a00301a90
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-03-06 13:49:54 +0900

    非単語形成文字でマッチさせるようにした．
    splitで引数なしの場合$; が使われ，初期値はnilなので 先頭，末尾の空白は除去された上でsplitされる
    see also: https://docs.ruby-lang.org/ja/latest/method/String/i/split.html

--- questions/q03.rb
+++ questions/q03.rb
@@ -1,2 +1,2 @@
 sentence = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-sentence.delete('.,').split.map(&:length)
+sentence.gsub(/\W+/, ' ').split.map(&:length)
