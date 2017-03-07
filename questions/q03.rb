cac64701a503cf3cee704a2538cc795ce7351b8d
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-03-07 09:36:09 +0900

    gsubが冗長のなので削除した

--- questions/q03.rb
+++ questions/q03.rb
@@ -1,2 +1,2 @@
 sentence = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-sentence.gsub(/\W+/, ' ').split.map(&:length)
+sentence.split(/\W+/).map(&:length)
