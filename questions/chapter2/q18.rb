0449f246ab1773731fda835c33a0bf99b0b37ce9
kyohei-shimada <kyohei-shimada@users.noreply.github.com>
2017-03-14 17:14:27 +0900

    oops

--- questions/chapter2/q18.rb
+++ questions/chapter2/q18.rb
@@ -2,4 +2,4 @@
 require 'csv'
 
 table = CSV.new($stdin, col_sep: "\t").read
-puts table.sort {|row1, row2| row2[2] <=> row1[2] }.map {|row| row.join("\t") }
+puts table.sort {|row1, row2| row2[2].to_f <=> row1[2].to_f }.map {|row| row.join("\t") }
